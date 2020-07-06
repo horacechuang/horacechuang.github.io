2ce5e5bc1cee5b29b513426ea251ab40fab136ef
Horace <tingfeng.chuang@gmail.com>
2020-07-06 11:25:51 +0800

    20200706_1125

---
+++ v2/js/hideMenu.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    
+     var c, currentScrollTop = 0,
+         navigation = $('nav');
+  
+     $(window).scroll(function () {
+        var a = $(window).scrollTop();
+        var b = navigation.height();
+       
+        currentScrollTop = a;
+       
+        if (c < currentScrollTop && a > b < b) {
+          navigation.addClass("scrollUp");
+        } else if (c > currentScrollTop && !(a <= b)) {
+          navigation.removeClass("scrollUp");
+        }
+        c = currentScrollTop;
+    });
+    
+  });
