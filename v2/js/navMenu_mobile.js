3c3cf3693402cc9edb638e821933f2b977f7b93f
Horace <tingfeng.chuang@gmail.com>
2020-07-29 18:16:29 +0800

    20200729_1816

--- v2/js/navMenu_mobile.js
+++ v2/js/navMenu_mobile.js
@@ -1,13 +1,10 @@
 $(function() {
-  
     $('.menuBtn').on('click', function() {
       $(this).toggleClass('btnActive');
       $(".nav-menu").toggleClass('collapse');
     })
-
     $('.btn-nav a').on('click', function() {
       $(".nav-menu").removeClass('collapse');
       $(".menuBtn").removeClass('btnActive');
     })
-
   });
