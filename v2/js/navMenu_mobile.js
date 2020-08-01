0d26c44d5bdfa75711d53b6122b3932aed2557e1
Horace <tingfeng.chuang@gmail.com>
2020-08-01 21:44:52 +0800

    20200801_2144

--- v2/js/navMenu_mobile.js
+++ v2/js/navMenu_mobile.js
@@ -2,12 +2,12 @@ $(function() {
     $('.menuBtn').on('click', function() {
       $(this).toggleClass('btnActive');
       $(".nav-menu").toggleClass('collapse');
-      $('html').toggleClass('scrollHidden');
+      $('body').toggleClass('scrollHidden');
     })
 
     $('.btn-nav a').on('click', function() {
       $(".menuBtn").removeClass('btnActive');
       $(".nav-menu").removeClass('collapse');
-      $('html').removeClass('scrollHidden');
+      $('body').removeClass('scrollHidden');
     })
   });
