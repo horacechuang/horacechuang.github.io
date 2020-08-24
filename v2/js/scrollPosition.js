3364fc99878bba83c4e3b87e61280e81f385181b
Horace Chuang <tingfeng.chuang@gmail.com>
2020-08-24 14:20:30 +0800

    20200824_1420

--- v2/js/scrollPosition.js
+++ v2/js/scrollPosition.js
@@ -1,7 +1,7 @@
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
 
-    if (scroll >= 125) {
+    if (scroll >= 80) {
         $("nav").addClass("addShadow");
     } else {
         $("nav").removeClass("addShadow");
