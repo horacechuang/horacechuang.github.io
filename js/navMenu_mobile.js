6ef9f5e6c96df5abe54853482c1a03657f46b9e5
Horace Chuang <tingfeng.chuang@gmail>
2022-03-22 18:17:34 +0800

    20220322_1817

--- js/navMenu_mobile.js
+++ js/navMenu_mobile.js
@@ -1,13 +1,17 @@
   $(function() {
-    $("nav#nav .mobileBtn").on('click', function() {
+    $("nav#nav .nav-btn").on('click', function() {
       $(this).toggleClass('btnActive');
-      $("nav#nav .item").toggleClass('collapse');
+      $("nav#nav .nav-container").toggleClass('collapse');
+      $("nav#nav .nav-column").toggleClass('collapse');
+      $("nav#nav .nav-item").toggleClass('collapse');
       $('html').toggleClass('scrollHidden');
     })
 
     $("nav#nav ul.container a").on('click', function() {
-      $("nav#nav .mobileBtn").removeClass('btnActive');
-      $("nav#nav .item").removeClass('collapse');
+      $("nav#nav .nav-btn").removeClass('btnActive');
+      $("nav#nav .nav-container").removeClass('collapse');
+      $("nav#nav .nav-column").removeClass('collapse');
+      $("nav#nav .nav-item").removeClass('collapse');
       $('html').removeClass('scrollHidden');
     })
 
